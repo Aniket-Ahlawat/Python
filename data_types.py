14ebe7ad27d9ab9aa27293cfea9a1bd1d8f040b4
Aniket Ahlawat <aniketahlawat495@gmail.com>
2026-04-29 21:30:28 +0530

    data types

---
+++ data_types.py
@@ -0,0 +1,19 @@
+ # basic types (integer , float, complex, boolean , string )
+ # container type (list, tuple,set and dictionary)
+ # user defined class
+
+
+print(12)
+print(1.23)
+print(True)
+print(4+5j)
+print("Rohtak")
+print('sonipat')
+
+# container type
+
+print([1,2,3,4,5,6])
+print((1,2,3,4))
+print({1,2,3,4,5})
+print({'name':'Aniket','age':23})
+
